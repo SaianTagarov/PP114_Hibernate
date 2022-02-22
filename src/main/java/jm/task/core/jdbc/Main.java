9b8a5e5e5c7342c1449cb24ab30ada1ca5224828
SaianTagarov <chitenggatab@gmail.com>
2022-02-22 22:28:42 +0900

    практическое задание Тагаров СА

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl usi = new UserServiceImpl();
+        usi.createUsersTable();
+
+        usi.saveUser("Пётр", "Петров", (byte) 25);
+        usi.saveUser("Иван", "Иванов", (byte) 35);
+        usi.saveUser("Василий", "Васильев", (byte) 52);
+        usi.saveUser("Алексей", "Алексеев", (byte) 39);
+
+        List<User> userList = usi.getAllUsers();
+        userList.stream().forEach(System.out::println);
+
+        //usi.removeUserById((long)4);
+        usi.cleanUsersTable();
+        usi.dropUsersTable();
+        usi.closeConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // реализуйте настройку соединения с БД
+    public static Connection getMyConnection() throws SQLException,
+            ClassNotFoundException {
+        String hostName = "localhost";
+        String dbName = "task1_db";
+        String userName = "root";
+        String password = "root21";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+        return DriverManager.getConnection(connectionURL, userName, password);
+    }
 }
